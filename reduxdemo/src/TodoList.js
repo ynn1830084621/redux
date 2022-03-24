b4a4443548f13e7715078cd49280f760223e355a
ynn <1830084621@qq.com>
2022-03-24 22:51:45 +0800

    "补充redux制作列表和Todolist删除功能"

--- reduxdemo/src/TodoList.js
+++ reduxdemo/src/TodoList.js
@@ -9,7 +9,8 @@ class TodoList extends Component {
         this.state = store.getState();
         this.changeInputValue = this.changeInputValue.bind(this);
         this.storeChange = this.storeChange.bind(this);
-        store.subscribe(this.storeChange)
+        this.clickBtn = this.clickBtn.bind(this);
+        store.subscribe(this.storeChange)//订阅redux的状态
         // console.log(this.state);
     }
     render() { 
@@ -17,17 +18,21 @@ class TodoList extends Component {
             <div>
                 <div style={{margin: '20px'}}>
                     <Input 
-                        placeholder={this.state.inputValue}
+                        placeholder='Write Something'
                         style={{width: '500px', marginRight: '10px'}}
                         onChange={this.changeInputValue}
+                        value={this.state.inputValue}
                     />
-                    <Button type='primary'>增加</Button>
+                    <Button 
+                        type='primary'
+                        onClick={this.clickBtn}
+                    >增加</Button>
                 </div>
                 <div style={{width: '600px', margin: '20px'}}>
                     <List
                         bordered
                         dataSource={this.state.list}
-                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                        renderItem={(item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)} 
                     />
                 </div>
             </div>
@@ -39,6 +44,17 @@ class TodoList extends Component {
             type : 'changeInput',
             value : e.target.value
         }
+        store.dispatch(action)
+    }
+    clickBtn() {
+        const action = { type :'addItem' };
+        store.dispatch(action)
+    }
+    deleteItem(index) {
+        const action = { 
+            type : 'deleteItem',
+            index,
+        };
         store.dispatch(action)
     }
     storeChange(){
--- reduxdemo/src/store/reducer.js
+++ reduxdemo/src/store/reducer.js
@@ -1,19 +1,31 @@
 const defaultState = {
-    inputValue : 'Write Something',
+    inputValue : '',
     list : [
         '上午学习英语',
         '下午学习react',
         '晚上敲代码',
     ]
 }
-export default (state = defaultState, action) => {
-    {
-        if(action.type === 'changeInput') {
-            let newState = JSON.parse(JSON.stringify(state));
-            newState.inputValue = action.value;
-            return newState;
-
-        }
+let reducerObj = (state = defaultState, action) => {
+    //console.log(state, action);
+    //reducer只能接收state不能改变state
+    if(action.type === 'changeInput') {
+        let newState = JSON.parse(JSON.stringify(state));//深度拷贝state
+        newState.inputValue = action.value;
+        console.log(state, newState, 'change')
+        return newState;
+    }
+    if(action.type === 'addItem') {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);//push新内容到列表
+        newState.inputValue = '';
+        return newState;
+    }
+    if(action.type === 'deleteItem') {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index, 1);
+        return newState;
     }
     return state;
 }
+export default reducerObj;

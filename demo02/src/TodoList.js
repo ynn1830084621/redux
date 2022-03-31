076cbfa524c02f708bead45341ba6c90a511cf98
ynn <1830084621@qq.com>
2022-03-31 23:40:23 +0800

    "用react-redux 写TodoList"

---
+++ demo02/src/TodoList.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import store from './store';
+import { connect } from 'react-redux';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+    }
+    render() { 
+        return ( 
+            <div>
+                <div>
+                    <input 
+                        value={this.props.inputValue}
+                        onChange={this.props.inputChange}
+                    />
+                    <button onClick={this.props.clickButton}>提交</button>
+                </div>
+                <ul>
+                    {
+                        this.props.list.map((item, index) => {
+                            return(<li key={index} onClick={() => this.props.deleteItem(index)}>{item}</li>)
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
+const stateToProps = (state) => {
+    return {
+        inputValue : state.inputValue,
+        list : state.list,
+    }
+}
+const dispatchToProps = (dispatch) => {
+    return {
+        inputChange(e) {
+            //console.log(e.target.value);
+            let action = {
+                type : 'change_input',
+                value : e.target.value,
+            }
+            dispatch(action)
+        },
+        clickButton() {
+            let action = {type : 'add_item'};
+            dispatch(action);
+        },
+        deleteItem(index) {
+            let action = {
+                type : 'delete_item',
+                index,
+            }
+            dispatch(action);
+        }
+    }
+}
+ 
+export default connect( stateToProps, dispatchToProps )(TodoList);

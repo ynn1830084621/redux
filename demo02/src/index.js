931bbf927dafb9c430f04ca25c21a4d9176ff24c
ynn <1830084621@qq.com>
2022-04-02 22:58:35 +0800

    "更改"

--- demo02/src/index.js
+++ demo02/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import TodoList from './TodoList';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,6 +11,6 @@ const App = (
 )
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container)
-root.render(App)
-//ReactDOM.render( App, document.getElementById('root'));
+root.render(App);
+
 
